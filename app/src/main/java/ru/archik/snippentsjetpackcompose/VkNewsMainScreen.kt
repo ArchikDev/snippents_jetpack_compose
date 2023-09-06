df77d95b64c51e47d3345080e427683cd2606303
a.galeev <a.galeev@malltech.ru>
2023-09-06 22:16:39 +0500

    настройки навигации

--- app/src/main/java/ru/archik/snippentsjetpackcompose/VkNewsMainScreen.kt
+++ app/src/main/java/ru/archik/snippentsjetpackcompose/VkNewsMainScreen.kt
@@ -5,12 +5,14 @@ import androidx.annotation.RequiresApi
 import androidx.compose.foundation.clickable
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import ru.archik.snippentsjetpackcompose.navigation.AppNavGraph
+import ru.archik.snippentsjetpackcompose.navigation.Screen
 
 @RequiresApi(Build.VERSION_CODES.N)
 @Composable
@@ -31,7 +33,17 @@ fun MainScreen(viewModel: MainViewModel) {
         items.forEach { item ->
           BottomNavigationItem(
             selected = currentRout == item.screen.route,
-            onClick = { navHostController.navigate(item.screen.route) },
+            onClick = {
+              navHostController.navigate(item.screen.route) {
+                launchSingleTop = true // не пересоздается экран если он текущий
+                // из бэкстэка удаляем все экраны до стартого экрана
+                popUpTo(Screen.NewsFeed.route) {
+                  // state сохраняется когда экраны удаляются из бэкстэка
+                  saveState = true
+                } // история переходов не хранится в приложении
+                restoreState = true // восстановление state после удаления экрана
+              }
+            },
             icon = {
               Icon(item.icon, contentDescription = null)
             },
@@ -61,7 +73,9 @@ fun MainScreen(viewModel: MainViewModel) {
 
 @Composable
 private fun TextCounter(name: String) {
-  var count by remember {
+  // remember - не сохраняется при рекомпозиции
+  // rememberSaveable - сохраняется при рекомпозиции(например переворот экрана)
+  var count by rememberSaveable {
     mutableStateOf(0)
   }
 
