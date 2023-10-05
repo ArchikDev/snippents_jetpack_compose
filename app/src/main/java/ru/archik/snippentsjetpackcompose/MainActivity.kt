cfcca1d6429d83b0491daf08c0f5318196c350f4
a.galeev <a.galeev@malltech.ru>
2023-10-05 17:30:19 +0500

    Авторизация через VK SDK

--- app/src/main/java/ru/archik/snippentsjetpackcompose/MainActivity.kt
+++ app/src/main/java/ru/archik/snippentsjetpackcompose/MainActivity.kt
@@ -2,7 +2,9 @@ package ru.archik.snippentsjetpackcompose
 
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
@@ -13,6 +15,9 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.vk.api.sdk.VK
+import com.vk.api.sdk.auth.VKAuthenticationResult
+import com.vk.api.sdk.auth.VKScope
 import ru.archik.snippentsjetpackcompose.ui.theme.SnippentsJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,8 +26,27 @@ class MainActivity : ComponentActivity() {
   @RequiresApi(Build.VERSION_CODES.N)
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+
+//    VK.login(activity, arrayListOf(VKScope.WALL, VKScope.PHOTOS))
+
     setContent {
       SnippentsJetpackComposeTheme {
+
+        val launcher = rememberLauncherForActivityResult(
+          contract = VK.getVKAuthActivityResultContract(),
+        ) {
+          when (it) {
+            is VKAuthenticationResult.Success -> {
+              Log.d("MainActivity", "success")
+            }
+            is VKAuthenticationResult.Failed -> {
+              Log.d("MainActivity", "error")
+            }
+          }
+        }
+
+        launcher.launch(listOf(VKScope.WALL))
+
         MainScreen(viewModel)
       }
     }
